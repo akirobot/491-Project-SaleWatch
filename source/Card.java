19cdc1220a50feb9918be4e5102d310d986b9ce0
Zubeir Said <Zubeir.Said@student.csulb.edu>
2017-04-25 15:56:32 -0700

    Update Card.java

--- source/Card.java
+++ source/Card.java
@@ -1,16 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package theilluminatigame;
+
+/**
+ *
+ * @author saidz
+ */
 /** Card is a representation of card */
 
 public class Card {
-
     /** The name of this card */
     private String name;
-
     /**
      * The type of this card. Types of Card (1): Illuminati Card (2): Group Card
      * (3): Special Card
      */
     private int type;
-
+    private int power;
+    private int transferPower;
+    private int income;
+    private int resistance;
+    
     /**
      * Special Card constructor
      * 
@@ -18,10 +31,9 @@ public class Card {
      * @param type - Must be type 3
      */
     public Card(String name, int type) {
-	this.name = name;
-	this.type = type;
+        this.name = name;
+        this.type = type;
     }
-
     /**
      * Illuminati Card constructor - An Illuminati Card does not have any
      * resistances
@@ -33,9 +45,13 @@ public class Card {
      * @param transferPower
      */
     public Card(String name, int type, int power, int transferPower, int income) {
-
+        this.name = name;
+        this.type = type;
+        this.power = power;
+        this.transferPower = transferPower;
+        this.income = income;
     }
-
+ 
     /**
      * Group Card constructor
      * 
@@ -46,29 +62,41 @@ public class Card {
      * @param transferPower
      * @param resistance
      */
-    public Card(String name, int type, int power, int transferPower,
-	    int resistance, int income) {
-
+    public Card(String name, int type, int power, int transferPower,int resistance, int income) {
+        this.name = name;
+        this.type = type;
+        this.power = power;
+        this.transferPower = transferPower;
+        this.resistance = resistance;
+        this.income = income;
     }
-
     public String getCardName() {
 	return name;
     }
-
     public int getCardType() {
-
 	if (type == 1) {
 	    System.out.println("This is an Illuminati card");
 	}
-
 	if (type == 2) {
 	    System.out.println("This is a Group card");
 	}
-
 	if (type == 3) {
 	    System.out.println("This is a Special card");
 	}
-
 	return type;
     }
+    public int getPower(){
+        return power;
+    }
+    public int getTransferPower(){
+        return transferPower;
+    }
+    public int getResistance(){
+        return resistance;
+    }
+    public int getIncome(){
+        return income;
+    }
 }
+
+           
