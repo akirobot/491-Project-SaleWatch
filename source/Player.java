f2e9bb4266537e43b620ed155105754aae1d245c
Patrick Ly <patlycsulb@gmail.com>
2017-04-25 09:05:07 -0700

    Worked on methods from Player class. Will add comments/Javadocs later

--- source/Player.java
+++ source/Player.java
@@ -1,54 +1,93 @@
 import java.util.ArrayList;
 
+/** Player is a representation of player */
 
 public class Player {
 
     private Illuminati illuminati;
     private int diceRollOutcome;
     private int actionsTaken;
-    private ArrayList<SpecialCard> specialCards = new ArrayList<SpecialCard>();
-    
-    public Player(Illuminati illuminati){
-	
+    private ArrayList<SpecialCard> specialCards;
+    private boolean interfereFlag;
+
+    public Player() {
+	diceRollOutcome = 0;
+	actionsTaken = 0;
+	specialCards = new ArrayList<SpecialCard>();
+	interfereFlag = false;
     }
-    
-    public Illuminati getIlluminati(){
+
+    public Player(Illuminati illuminati) {
+	this.illuminati = illuminati;
+	diceRollOutcome = 0;
+	actionsTaken = 0;
+	specialCards = new ArrayList<SpecialCard>();
+	interfereFlag = false;
+    }
+
+    public void setIlluminati(Illuminati illuminati) {
+	this.illuminati = illuminati;
+    }
+
+    public Illuminati getIlluminati() {
 	return this.illuminati;
     }
-    
-    public void addSpecialCard(){
-	
+
+    public void addSpecialCard(SpecialCard specialCard) {
+	specialCards.add(specialCard);
     }
-    public void useSpecialCard(){
-	
+
+    public void useSpecialCard(int choice) {
+	specialCards.get(choice).useSpecialCardEffect();
     }
-    
-    public void rollDie(){
-	
+
+    // Roll dice value from 2 to 12
+    public void rollDie() {
+
+	int highestRange = 12;
+	int lowestRange = 2;
+	diceRollOutcome = (int) (Math.random() * highestRange) + lowestRange;
+
     }
-    
-    public int getDiceRollOutcome(){
+
+    public int getDiceRollOutcome() {
 	return diceRollOutcome;
     }
-    
-    public int getActionsTaken(){
+
+    public int getActionsTaken() {
 	return actionsTaken;
     }
-    
-    public void takeAction(){
-	
+
+    public void takeAction() {
+	actionsTaken--;
     }
-    
-    public void resetAction(){
-	
+
+    /**
+     * After the player is done with his turn, then his number of actions taken
+     * is set back to 0
+     */
+    public void resetAction() {
+	actionsTaken = 0;
     }
-    
-    public void interfere(){
-	
+
+    /** Player interferes an attack */
+    public void interfere() {
+	interfereFlag = true;
     }
-    
-    public void displayPowerStructure(){
-	
+
+    /**
+     * After the player is done with his turn, then his interference is set back
+     * to false
+     */
+    public void resetInterfere() {
+	interfereFlag = false;
     }
-    
+
+    /**
+     * Remove this method since the power structured should be already be
+     * displayed via the gameUI class public void displayPowerStructure(){
+     * 
+     * }
+     */
+
 }
