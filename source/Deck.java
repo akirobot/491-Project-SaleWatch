f9cf527b39cb1f5b33d298e8019ad2363c3fac94
anonymous <yoyuo@yopmail.com>
2017-04-25 07:14:29 -0700

    Worked on Deck class methods.  Comments were added as well.

--- source/Deck.java
+++ source/Deck.java
@@ -1,22 +1,36 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
+
+/**
+Deck is an abstract representation of Deck
+*/
 public class Deck {
 
-    private ArrayList<Card> deck = new ArrayList<Card>();
+    // Private member called deck, which is an ArrayList that takes in a Card object
+    // Group Cards, Special Cards are subclasses of Cards, so they can go inside deck.
+
+    private ArrayList<Card> deck;
    
-    public Deck(){
-	
+    // Default constructor
+    public Deck(){s
+	   deck = new ArrayList<Card>();
     }
     
+    // Add card to deck 
     public void addCards(Card card){
-	deck.add(card);
+	   deck.add(card);
     }
+
+    // Shuffle the deck
     public void shuffleDeck(){
-	    
+        Collections.shuffle(this.deck);
     }
     
+    /* Remove this method, since it's not needed
     public void displayAllCards(){
 	
     }
+    */
 }
 
