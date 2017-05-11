02763891ea87a3a4a2745cf7687a618d91a2c41a
patrickly <patrickkimly@gmail.com>
2017-05-10 18:03:17 -0700

    Remove folders for repo

--- tempSource/cardUpdateStats.java
+++
@@ -1,7 +0,0 @@
-package theilluminatigame;
-
-public interface cardUpdateStats {
-    public void adjustTreasury(int megaBucks);
-    public void adjustPower(int power);
-    public void adjustTransferrablePower(int tPower);
-}
